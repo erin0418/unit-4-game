0b204441355ee32dc86553a7747573542d3eaa09
Erin0418 <erinf0418@gmail.com>
2018-06-16 23:18:57 -0400

    can't get it to reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,28 @@
-var win = false;
-var randomNumber = 0;
 var yourNumber = 0;
-
+var randomNumber = Math.floor(Math.random() * 100);
 var treasure = (Math.floor(Math.random() * 12) +1);
 var mushroom = (Math.floor(Math.random() * 12) +1);
 var coin = (Math.floor(Math.random() * 12)+1);
 var bag = (Math.floor(Math.random() * 12)+1);
 
+$(".yourNumber").append(yourNumber);
+$(".randomNumber").append(randomNumber);
+
 $("#bag").on("click", function (){
-    $(".yournumber").html(bag);
+    yourNumber=(yourNumber + bag);
+    $(".yourNumber").html(yourNumber);
 }) 
+$("#treasure").on("click", function (){
+    yourNumber=(yourNumber + treasure);
+    $(".yourNumber").html(yourNumber);
+})
+$("#coin").on("click", function (){
+    yourNumber=(yourNumber + coin);
+    $(".yourNumber").html(yourNumber);
+})
+$("#mushroom").on("click", function (){
+    yourNumber=(yourNumber + mushroom);
+    $(".yourNumber").html(yourNumber);
+})
 
-if (win === false) {
-        randomNumber = Math.floor(Math.random() * 100);
-        $(".randomNumber").append(randomNumber);
-    }
 
-    if (randomNumber === yourNumber) {
-        win = true;
-    }
-
-
-   console.log(bag,treasure,coin,mushroom)
